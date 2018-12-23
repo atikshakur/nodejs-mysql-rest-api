f0d563d3c7b999fff160e1b67168575e8437846c
Atik Shakur <atik.shakur98@gmail.com>
2018-12-23 21:24:22 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+app.use(morgan("short"))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static('./post-news'))
+
+app.get("/", (req, res) => {
+    console.log("Hello from root");
+    res.send("Hello from rooooooot")
+})
+
+const latestNewsRoute = require('./routes/latest.js')
+app.use(latestNewsRoute)
+
+const trendingNewsRoute = require('./routes/trending.js')
+app.use(trendingNewsRoute)
+
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => console.log("Server is up and running..."))
---
+++ routes/latest.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const mysql = require('mysql')
+const router = express.Router()
+
+router.get("/latest", (req, res) => {
+    console.log("Responding to LATEST NEWS route");
+
+    getConnection().query("SELECT * FROM latest", (error, rows, fields) => {
+        console.log('Running sql queries')
+        if (error) throw error
+
+        res.json(rows)
+    })
+})
+
+router.post('/post-latest-submitted', (req, res) => {
+    console.log('title:' + req.body.submit_title);
+
+    let title = req.body.submit_title
+    let writter = req.body.submit_writter
+    let category = req.body.submit_category
+    let imageLink = req.body.submit_image_link
+    let news1 = req.body.submit_news1
+    let news2 = req.body.submit_news2
+    let news3 = req.body.submit_news3
+
+    const queryString = "INSERT INTO latest (title, writter, category, image_link, news1, news2, news3) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    getConnection().query(queryString, [title, writter, category, imageLink, news1, news2, news3], (error, results, fields) => {
+        if (error) {
+            console.log("Inserting error: " + error);
+            res.sendStatus(500)
+            return
+        }
+        res.end()
+    })
+})
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'Schema here',
+    user: 'root',
+    password: 'root',
+    database: 'DB link here'
+})
+
+function getConnection() {
+    return pool
+}
+
+module.exports = router
